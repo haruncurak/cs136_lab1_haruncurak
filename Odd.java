926ab34edea30864ed9f59ba702ca05ded3b9fcd
23hc9 <23hc9@tcl217a-4.cs.williams.edu>
2020-02-12 12:43:07 -0500

    draft

---
+++ Odd.java
@@ -0,0 +1,12 @@
+public class Odd {
+
+
+  public static void main(String[] args){
+    int initialNumber = 0;
+
+    for(int i = 0; i < 10; i++) {
+      System.out.println(2 * initialNumber + 1);
+      initialNumber ++;
+    }
+  }
+}
